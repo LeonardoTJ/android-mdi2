4de4c7c107a223ea6f015daf03874dfc5efa3f3e
Leonardo Melendez <leonardo.melendez@uabc.edu.mx>
2024-11-29 15:54:02 -0800

    refactor 112 class 1 assignment code, change to compose

--- 112/assignments/class-1/class112_1.kt
+++ 112/assignments/class-1/class112_1.kt
@@ -1,13 +1,48 @@
 class MainActivity : ComponentActivity() {
-    private var steps: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setTheme(android.R.style.Theme_DeviceDefault)
+        setContent {
+            StepCounterApp()
+        }
 
-        steps = 30
-        val stepCountTextView: TextView = findViewById(R.id.step_count_text)
-        stepCountTextView.text = "Steps: $steps."
     }
+}
 
+@Composable
+fun StepCounterApp() {
+    MaterialTheme {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            StepCounterScreen(stepCount = 1234) // Predefined step count
+        }
+    }
+}
+
+@Composable
+fun StepCounterScreen(stepCount: Int) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Step Count",
+            style = MaterialTheme.typography.body1,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = "$stepCount",
+            style = MaterialTheme.typography.display1,
+            fontSize = 48.sp,
+            fontWeight = FontWeight.ExtraBold
+        )
+    }
 }
